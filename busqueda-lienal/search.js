7f4b6e2b51a34285f1b8faa1849291b60422f9f1
prox198 <162045055+prox198@users.noreply.github.com>
2026-09-23 22:55:44 -0600

    Add linear search implementation in search.js
    Implement linear search function for linked list.

--- busqueda-lienal/search.js
+++ busqueda-lienal/search.js
@@ -1,1 +1,30 @@
-search.js
+const Node = require('./node');
+
+function linearSearch(head, el) {
+   let current = head;
+
+  while (current !== null) {
+    if (current.value === el) {
+      return true;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return false;
+}
+
+
+const nodeOne = new Node(7);
+const nodeTwo = new Node(2);
+const nodeThree = new Node(8);
+const nodeFour = new Node(4);
+
+nodeOne.next = nodeTwo;
+nodeTwo.next = nodeThree;
+nodeThree.next = nodeFour;
+
+console.log(linearSearch(nodeOne, 2)); // true
+console.log(linearSearch(nodeOne, 7)); // true
+console.log(linearSearch(nodeOne, 8)); // true
+console.log(linearSearch(nodeOne, 10)); // false
